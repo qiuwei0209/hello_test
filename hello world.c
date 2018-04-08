529c67cfc25cbbb6e88694822d80f3de7dea8f2c
qiuwei0209 <qiuwei0209@163.com>
2018-04-08 15:52:50 +0800

    Edit by debian 9.2

--- hello world.c
+++ hello world.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
+int test_out(void)
+{
+    printf("hello world!");
+}
+
 int main(void)
 {
     printf("hello world!");
-
+    test_out();
     return 0;
 }
